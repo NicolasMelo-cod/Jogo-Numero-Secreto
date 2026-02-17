ab0ac28e4be9b21dfd6ac8b842980a38c0e7f226
NicolasMelo-cod <nicolasnascimento325@gmail.com>
2026-02-17 09:41:45 -0300

    push

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-// enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 100');
-    // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
     } else {
@@ -16,7 +14,6 @@ while (chute != numeroSecreto) {
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-        // tentativas = tentativas + 1;
         tentativas++;
     }
 }
